0ca892a6d2dfc77de36bddc7e33cd7ccf2907a94
Aline <alinecris@gmail.com>
2020-09-03 18:05:44 +0200

    Install and configure Jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)'],
+  setupFileAfterEnv: ['<rootDir>/.jest/setup.ts']
+}
