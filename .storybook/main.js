856d312e155cc8cc8c66bf8c59411304d1d94fb5
Aline <alinecris@gmail.com>
2020-10-07 21:29:09 +0200

    Add storybook essentials

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,4 @@
 module.exports = {
   "stories": ["../src/components/**/stories.tsx"],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-knobs/register"
-  ]
+  "addons": ["@storybook/addon-essentials"]
 }
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,19 +1,9 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { withKnobs, text } from '@storybook/addon-knobs'
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'Advanced React')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS and Styled Components'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
